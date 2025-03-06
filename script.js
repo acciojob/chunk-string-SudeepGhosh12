f39ed5a5faa42b566532351ba8dea126cfcfc13c
Sudeep Ghosh <133862916+SudeepGhosh12@users.noreply.github.com>
2025-03-06 10:10:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 function stringChop(str, size) {
   // your code here
+	
+	
+	if (str === null) {
+    return [];
+  }
+
+
+  const chunks = [];
+
+  
+  for (let i = 0; i < str.length; i += size) {
+    // Extract a chunk of the specified size
+    const chunk = str.slice(i, i + size);
+    // Add the chunk to the array
+    chunks.push(chunk);
+  }
+
+
+  return chunks;
 }
+
 
 // Do not change the code below
 const str = prompt("Enter String.");
